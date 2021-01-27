bb682cb6fd735593c112eeabda303126a5240f11
Aleksander <alexzlow@gmail.com>
2021-01-27 04:14:46 +0300

    new

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,9 +1,9 @@
 // import React, { Component } from 'react'
 
 
-export default () => (
+export default (props) => (
   <div>
-    <h3>Car name: {'CAR_NAME'}</h3>
-    <h3>Car year: {'2018'}</h3>
+    <h3>Car name: {props.name}</h3>
+    <h3>Car year: {props.year}</h3>
   </div>
 )
